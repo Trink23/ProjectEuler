a40104a103b22f86afbf630073976686c94b32a2
guiller anguita <guiller3334@outlook.com>
2022-04-21 16:28:32 +0100

    Added MAUI now projectEuler will run on windows application android and IOS

---
+++ MauiProjectEuler/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using MauiProjectEuler.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace MauiProjectEuler
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
